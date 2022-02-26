db57f44063e75651d7a1ab68c2aff7dd1ff6dab4
Angel Contreras Dueñas <c3po.angel@gmail.com>
2022-02-25 17:35:56 -0700

    modifs

--- App/main.py
+++ App/main.py
@@ -9,60 +9,54 @@ import time #usar un Delay
 client = MongoClient(host='localhost', port=27017)
 
 print("ejecutando...")
-limite = 0
 
-while True:
+#conectar a la base de datos
+db = client['Reg_Radiacion_Metro']
+#genera la coleccion
+linea_roja = db['Linea_Roja'] 
+hanza = db['Hanza']
+estaciones_independientes = db['Estaciones_Independientes']
+
+for i in range(990):
     #Simulacion de la lectura de la temperatura
     rad1 = random.randint(0, 500)
     rad2 = random.randint(100, 1500)
     rad3 = random.randint(0, 100)
-    #genera objeto Json con los datos de interes
-    valor1 = {
+    #genera objeto  con los datos de interes
+    VALOR_1 = {
         'Nombre':'Linea Roja',
         'Nivel de Peligro':'Alto',
         'Estaciones':'16',
         'Rediacion':rad1
     }
-
-    valor2 = {
+    VALOR_2 = {
         'Nombre':'Conjunto de estaciones independientes',
         'Nivel de Peligro':'Indeterminable',
         'Estaciones':'6',
         'Rediacion':rad2
     }
-
-    valor3 = {
+    VALOR_3 = {
         'Nombre':'Hanza',
         'Nivel de Peligro':'Bajo',
         'Estaciones':'20',
         'Rediacion':rad3
     }
 
-    #conectar a la base de datos
-    db = client['Reg_Radiacion_Metro']
-    #genera la coleccion
-    coleccion_1_data = db['Linea_Roja']
+   
+  
+    linea_roja.insert_one(VALOR_1)
+  
+    print('LR:', VALOR_1)
+    
     #inserta un dato en la coleccion
-    coleccion_1_data.insert_one(valor1)
-    #presentacion
-    print('LR:', valor1)
+    estaciones_independientes.insert_one(VALOR_2)
+    print('EI', VALOR_2)
 
-    coleccion_2_data = db['Estaciones_Independientes']
+   
     #inserta un dato en la coleccion
-    coleccion_2_data.insert_one(valor2)
-    #presentacion
-    print('EI', valor2)
-
-    coleccion_3_data = db['Hanza']
-    #inserta un dato en la coleccion
-    coleccion_3_data.insert_one(valor3)
-    #presentacion
-    print('H', valor3)
+    hanza.insert_one(VALOR_3)
+  
+    print('H', VALOR_3)
 
     #añadir un delay de medio segundo
     time.sleep(0.1)
-
-    limite = limite + 1
-
-    if limite == 990:
-        break
